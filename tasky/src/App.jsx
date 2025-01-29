163c1545f2ab6a23eaf2b023f949a06d656b1cc9
Shannon <xXSHANNONTXx@hotmail.com>
2025-01-29 15:52:38 +0000

    added Delete button

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -18,6 +18,11 @@ function App() {
     tasks[taskIndex].done = !tasks[taskIndex].done;
     setTaskState({ tasks });
   }
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
 
 
   return (
@@ -31,6 +36,8 @@ function App() {
       key={task.id}
       done={task.done}
       markDone={() => doneHandler(index)}
+      deleteTask = {() => deleteHandler(index)}
+
     />
   ))}
 
