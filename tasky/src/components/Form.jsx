53a0238bcf69f32b588bcc105fe4820908215676
Shannon <xXSHANNONTXx@hotmail.com>
2025-01-29 16:57:02 +0000

    added drop down

--- tasky/src/components/Form.jsx
+++ tasky/src/components/Form.jsx
@@ -4,34 +4,39 @@ const AddTaskForm = (props) => {
 
   return (
     <div>
-          <form onSubmit={props.submit}>
+      <form onSubmit={props.submit}>
 
         <label>
-            Task title:
-            <input type="text" name="title" required onChange={(event) => props.change(event)}
- />
+          Task title:
+          <input type="text" name="title" required onChange={(event) => props.change(event)}
+          />
         </label>
         <br />
         <label>
-            Due date:
-            <input type="date" name="deadline" required onChange={(event) => props.change(event)}
- />
+          Due date:
+          <input type="date" name="deadline" required onChange={(event) => props.change(event)}
+          />
         </label>
         <br />
         <label>
-            Details:
-            <input type="text" name="description" onChange={(event) => props.change(event)}
- />
- </label>
+          Details:
+          <input type="text" name="description" onChange={(event) => props.change(event)}
+          />
+        </label>
         <br />
         <label>
-            Priority Level:
-            <input type="text" name="priority" onChange={(event) => props.change(event)}
- />
+          Priority:
+          <select type="text" name="priority" onChange={(event) => props.change(event)}
+          >
+          <option value="high">High</option>
+          <option value="medium">Medium</option>
+          <option value="low">Low</option> 
+          </select>
         </label>
+
         <input type="submit" value="Submit" onChange={(event) => props.change(event)}
- />
-        </form>
+        />
+      </form>
     </div>
   )
 };
